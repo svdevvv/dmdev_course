92a25c933ced730b759aa555b151545424dfc2de
svdevvv <sv.devvv@gmail.com>
2024-09-10 01:10:47 +0300

    Переделанное

--- Task2Switch.java
+++ Task2Switch.java
@@ -15,24 +15,43 @@ import java.util.Scanner;
 //Результат: 34.5 (24.4 + 10.1)
 
 public class Task2Switch {
+    public final static char ADDITION = '+';
+    public final static char SUBTRACTION = '-';
+    public final static char MULTIPLICATION = '*';
+    public final static char DIVISION = '/';
+    public final static char MODULUS = '%';
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
         double operand1 = scanner.nextDouble();
         double operand2 = scanner.nextDouble();
-        System.out.println(operation(operand1,operand2,'*'));
+        System.out.println(calculate(operand1,operand2,'+'));
 
     }
+    private static double addition(double operand1, double operand2) {
+        return operand1 + operand2;
+    }
+    private static double subtraction(double operand1, double operand2) {
+        return operand1 - operand2;
+    }
+    private static double multiplication(double operand1, double operand2) {
+        return operand1 * operand2;
+    }
+    private static double division(double operand1, double operand2) {
+        return operand1 / operand2;
+    }
+    private static double modulus(double operand1, double operand2) {
+        return operand1 % operand2;
+    }
 
-    public static double operation(double operand1, double operand2, char operation) {
-        double result = 0;
-        switch (operation) {
-            case '+' -> result = operand1 + operand2;
-            case '-' -> result = operand1 - operand2;
-            case '*' -> result = operand1 * operand2;
-            case '/' -> result = operand1 / operand2;
-            case '%' -> result = operand1 % operand2;
-            default -> result = 0;
-        }
-        return result;
+    private static double calculate(double operand1, double operand2, char operation) {
+        return switch (operation) {
+            case ADDITION -> addition(operand1, operand2);
+            case SUBTRACTION -> subtraction(operand1, operand2);
+            case MULTIPLICATION -> multiplication(operand1, operand2);
+            case DIVISION -> division(operand1, operand2);
+            case MODULUS -> modulus(operand1,  operand2);
+            default -> throw new IllegalArgumentException("Invalid mathematical operation");
+        };
     }
 }
